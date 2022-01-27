bc46ef00e69408f1670a8dcb47104dfc102c12e2
Tim <linzetian@huya.com>
2022-01-27 10:03:35 +0800

    添加git ignore

--- dist/helper/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOKEN = exports.COOKIE = void 0;
-exports.COOKIE = process.env.COOKIE || '';
-exports.TOKEN = process.env.TOKEN || '';
--- dist/helper/timer.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.randomSleep = void 0;
-const getRandomTime = (min, max) => (Math.floor(Math.random() * (max - min)) + min) * 1000;
-const randomSleep = () => new Promise(((resolve) => setTimeout(resolve, getRandomTime(2, 4))));
-exports.randomSleep = randomSleep;
